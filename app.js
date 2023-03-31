19501f61bce3c148ca615d9c87ded91de7226905
matthieubrunetdebaines <matbrudeb@gmail.com>
2023-03-31 06:31:05 +0200

    spanburger maj

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ const topSpan = document.querySelector(".topSpan");
 const middleSpan = document.querySelector(".middleSpan");
 const bottomSpan = document.querySelector(".bottomSpan");
 
+
 boutonMenu.addEventListener("click", () => {
     navBar.classList.toggle("setMenu");
     topSpan.classList.toggle("topSpanBye");
     middleSpan.classList.toggle("middleSpanBye");
     bottomSpan.classList.toggle("bottomSpanBye");
 })
+
+
+function checkPosition() {
+    const aboutMePosition = document.getElementById("contactMe").offsetTop;
+    const menuBurger = document.querySelectorAll(".spanBurger");
+    const currentPosition = window.innerHeight + window.scrollY;
+  
+    menuBurger.forEach((burger) => {
+      if (currentPosition >= aboutMePosition) {
+        burger.classList.add("change_color");
+      } else {
+        burger.classList.remove("change_color");
+      }
+    });
+  }
+  
+  window.addEventListener("scroll", checkPosition);
